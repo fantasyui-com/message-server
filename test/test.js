cd2f1edf3a4d3aba57c9ae7b7b62a6b37417f8dd
fantasyui-com <captain@fantasyui.com>
2018-05-03 22:14:52 -0400

    Table COunt Test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,38 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+
+// Database prep
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('messages');
+
+
+describe('Database Tests', function() {
+
+  describe('table base', function() {
+    it('tables should be present', function(done) {
+
+      const tableCount = 0;
+
+      db.serialize(function () {
+
+        db.each(
+          "SELECT * FROM sqlite_master where type='table'",
+          function(err, row) {
+            tableCount++;
+            console.log("User id : "+row.id, row.dt);
+          },
+          function() {
+            assert.equal(tableCount, 2);
+            done();
+          }
+        );
+
+
+
+      });
+
+      // db.close();
+
     });
   });
+
 });
